f3b6e677b4da414fc5c9708466d401230cae4aa8
Sylvain Laperche <sylvain.laperche@gmail.com>
2021-12-26 09:12:24 +0100

    roaring: simplify iterator implementation

--- src/roaring/iter.rs
+++ src/roaring/iter.rs
@@ -1,26 +1,25 @@
 use super::{Entry, Header};
 use crate::{chunk, Chunk};
 
+type ChunkFlatIter<'a> = std::iter::FlatMap<
+    std::slice::Iter<'a, Chunk<Header>>,
+    ChunkIter<'a>,
+    fn(&'a Chunk<Header>) -> ChunkIter<'a>,
+>;
+
 /// Immutable Roaring bitmap iterator.
 ///
 /// This struct is created by the `iter` method on Roaring bitmap.
 pub struct Iter<'a> {
-    inner: std::slice::Iter<'a, Chunk<Header>>,
-    iter: Option<ChunkIter<'a>>,
+    inner: ChunkFlatIter<'a>,
     size: usize,
 }
 
 impl<'a> Iter<'a> {
-    pub(super) fn new(mut chunks: std::slice::Iter<'a, Chunk<Header>>) -> Self {
-        let size = chunks
-            .clone()
-            .fold(0, |acc, chunk| acc + chunk.cardinality());
-        let iter = chunks.next().map(Into::into);
-
+    pub(super) fn new(chunks: std::slice::Iter<'a, Chunk<Header>>) -> Self {
         Self {
-            inner: chunks,
-            iter,
-            size,
+            inner: chunks.clone().flat_map(Into::into),
+            size: chunks.fold(0, |acc, chunk| acc + chunk.cardinality()),
         }
     }
 }
@@ -30,12 +29,7 @@ impl<'a> Iterator for Iter<'a> {
 
     fn next(&mut self) -> Option<u32> {
         self.size = self.size.saturating_sub(1);
-        let iter = self.iter.as_mut()?;
-
-        iter.next().or_else(|| {
-            self.iter = self.inner.next().map(Into::into);
-            self.iter.as_mut().and_then(std::iter::Iterator::next)
-        })
+        self.inner.next()
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
