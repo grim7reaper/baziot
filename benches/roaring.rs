389f556a2b3b51ff3f394b7d088094ad7baaae1a
Sylvain Laperche <sylvain.laperche@gmail.com>
2021-12-28 12:08:57 +0100

    bench: reduce duplicate/boilerplate (macro FTW)

--- benches/roaring.rs
+++ benches/roaring.rs
@@ -1,150 +1,237 @@
-use baziot::Roaring;
+use baziot::{Roaring, RoaringLazy, RoaringTreeMap, RoaringTwoLevels};
 use criterion::{
     black_box, criterion_group, criterion_main, BatchSize, BenchmarkId,
     Criterion,
 };
-use rand::{seq::SliceRandom, thread_rng, Rng};
+use rand::{
+    distributions::Standard, prelude::*, seq::SliceRandom, thread_rng, Rng,
+};
 
-fn insert(c: &mut Criterion) {
-    let mut group = c.benchmark_group("insert");
+macro_rules! bench_insert_loop {
+    // Benchmark insertion using a loop, with either sorted or unsorted input.
+    ($group:ident, $count:ident, $sorted:literal, $roaring: ty, $int:ty) => {
+        let id = stringify!($roaring);
+        let benchmark_id = if $sorted {
+            BenchmarkId::new(format!("{}/Loop/Sorted", id), $count)
+        } else {
+            BenchmarkId::new(format!("{}/Loop/Random", id), $count)
+        };
+        $group.bench_with_input(benchmark_id, $count, |b, &$count| {
+            let values: Vec<$int> = get_input_values($count, $sorted);
+            b.iter(|| {
+                let mut bitmap = <$roaring>::new();
+                for v in &values {
+                    bitmap.insert(*v);
+                }
+                bitmap
+            });
+        });
+    };
+}
+
+fn insert_loop(c: &mut Criterion) {
+    let mut group = c.benchmark_group("InsertLoop");
     for count in [1, 10, 100, 1_000, 10_000].iter() {
-        group.bench_with_input(
-            BenchmarkId::new("Loop/Random", count),
-            count,
-            |b, &count| {
-                let values = get_input_values(count, false);
-                b.iter(|| {
-                    let mut bitmap = Roaring::new();
-                    for v in &values {
-                        bitmap.insert(*v);
-                    }
-                    bitmap
-                });
-            },
-        );
-        group.bench_with_input(
-            BenchmarkId::new("Iter/Random", count),
-            count,
-            |b, &count| {
-                let values = get_input_values(count, false);
-                b.iter(|| values.iter().copied().collect::<Roaring>());
-            },
-        );
-        group.bench_with_input(
-            BenchmarkId::new("Loop/Sorted", count),
-            count,
-            |b, &count| {
-                let values = get_input_values(count, true);
-                b.iter(|| {
-                    let mut bitmap = Roaring::new();
-                    for v in &values {
-                        bitmap.insert(*v);
-                    }
-                    bitmap
-                });
-            },
-        );
-        group.bench_with_input(
-            BenchmarkId::new("Iter/Sorted", count),
-            count,
-            |b, &count| {
-                let values = get_input_values(count, true);
-                b.iter(|| values.iter().copied().collect::<Roaring>());
-            },
-        );
+        bench_insert_loop!(group, count, true, Roaring, u32);
+        bench_insert_loop!(group, count, false, Roaring, u32);
+
+        bench_insert_loop!(group, count, true, RoaringTwoLevels, u64);
+        bench_insert_loop!(group, count, false, RoaringTwoLevels, u64);
+
+        bench_insert_loop!(group, count, true, RoaringTreeMap, u64);
+        bench_insert_loop!(group, count, false, RoaringTreeMap, u64);
+
+        bench_insert_loop!(group, count, true, RoaringLazy, u64);
+        bench_insert_loop!(group, count, false, RoaringLazy, u64);
     }
     group.finish();
+}
+
+macro_rules! bench_insert_iter {
+    // Benchmark insertion using an iterator, with either sorted or unsorted
+    // input.
+    ($group:ident, $count:ident, $sorted: literal, $roaring: ty, $int:ty) => {
+        let id = stringify!($roaring);
+        let benchmark_id = if $sorted {
+            BenchmarkId::new(format!("{}/Iter/Sorted", id), $count)
+        } else {
+            BenchmarkId::new(format!("{}/Iter/Random", id), $count)
+        };
+        $group.bench_with_input(benchmark_id, $count, |b, &$count| {
+            let values: Vec<$int> = get_input_values($count, $sorted);
+            b.iter(|| values.iter().copied().collect::<$roaring>());
+        });
+    };
+}
+
+fn insert_iter(c: &mut Criterion) {
+    let mut group = c.benchmark_group("InsertIter");
+    for count in [1, 10, 100, 1_000, 10_000].iter() {
+        bench_insert_iter!(group, count, true, Roaring, u32);
+        bench_insert_iter!(group, count, false, Roaring, u32);
+
+        bench_insert_iter!(group, count, true, RoaringTwoLevels, u64);
+        bench_insert_iter!(group, count, false, RoaringTwoLevels, u64);
+
+        bench_insert_iter!(group, count, true, RoaringTreeMap, u64);
+        bench_insert_iter!(group, count, false, RoaringTreeMap, u64);
+
+        bench_insert_iter!(group, count, true, RoaringLazy, u64);
+        bench_insert_iter!(group, count, false, RoaringLazy, u64);
+    }
+    group.finish();
+}
+
+macro_rules! bench_contains {
+    // Benchmark membership test, with a value present or absent.
+    ($group:ident, $count:ident, $present: literal, $roaring: ty, $int:ty) => {
+        let benchmark_id = BenchmarkId::new(
+            format!("{}/{}", stringify!($roaring), stringify!($present)),
+            $count,
+        );
+        $group.bench_with_input(benchmark_id, $count, |b, &$count| {
+            let (bitmap, mut value) = random_bitmap::<$roaring, $int>($count);
+            if !$present {
+                value = bitmap.max().expect("non-empty bitmap") + 1;
+            }
+            b.iter(|| bitmap.contains(black_box(value)));
+        });
+    };
 }
 
 fn contains(c: &mut Criterion) {
-    let mut group = c.benchmark_group("contains");
+    let mut group = c.benchmark_group("Contains");
     for count in [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000].iter() {
-        group.bench_with_input(
-            BenchmarkId::new("True", count),
-            count,
-            |b, &count| {
-                let (bitmap, value) = random_bitmap(count);
-                b.iter(|| bitmap.contains(black_box(value)));
-            },
-        );
-        group.bench_with_input(
-            BenchmarkId::new("False", count),
-            count,
-            |b, &count| {
-                let (bitmap, _) = random_bitmap(count);
-                let value = bitmap.max().expect("non-empty bitmap") + 1;
-                b.iter(|| bitmap.contains(black_box(value)));
-            },
-        );
+        bench_contains!(group, count, true, Roaring, u32);
+        bench_contains!(group, count, false, Roaring, u32);
+
+        bench_contains!(group, count, true, RoaringTwoLevels, u64);
+        bench_contains!(group, count, false, RoaringTwoLevels, u64);
+
+        bench_contains!(group, count, true, RoaringTreeMap, u64);
+        bench_contains!(group, count, false, RoaringTreeMap, u64);
+
+        bench_contains!(group, count, true, RoaringLazy, u64);
+        bench_contains!(group, count, false, RoaringLazy, u64);
     }
     group.finish();
 }
 
-fn cardinality(c: &mut Criterion) {
-    let mut group = c.benchmark_group("cardinality");
-    for count in [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000].iter() {
-        group.bench_with_input(
-            BenchmarkId::from_parameter(count),
-            count,
-            |b, &count| {
-                let (bitmap, _) = random_bitmap(count);
+macro_rules! bench_cardinality {
+    // Benchmark cardinality computation.
+    ($group:ident, $count:ident, $roaring: ty, $int:ty) => {
+        $group.bench_with_input(
+            BenchmarkId::new(stringify!($roaring), $count),
+            $count,
+            |b, &$count| {
+                let (bitmap, _) = random_bitmap::<$roaring, $int>($count);
                 b.iter(|| bitmap.cardinality());
             },
         );
+    };
+}
+
+fn cardinality(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Cardinality");
+    for count in [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000].iter() {
+        bench_cardinality!(group, count, Roaring, u32);
+        bench_cardinality!(group, count, RoaringTwoLevels, u64);
+        bench_cardinality!(group, count, RoaringTreeMap, u64);
+        bench_cardinality!(group, count, RoaringLazy, u64);
     }
     group.finish();
 }
 
-fn is_empty(c: &mut Criterion) {
-    let mut group = c.benchmark_group("is_empty");
-    for count in [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000].iter() {
-        group.bench_with_input(
-            BenchmarkId::from_parameter(count),
-            count,
-            |b, &count| {
-                let (bitmap, _) = random_bitmap(count);
+macro_rules! bench_is_empty {
+    // Benchmark the emptyness test.
+    ($group:ident, $count:ident, $roaring: ty, $int:ty) => {
+        $group.bench_with_input(
+            BenchmarkId::new(stringify!($roaring), $count),
+            $count,
+            |b, &$count| {
+                let (bitmap, _) = random_bitmap::<$roaring, $int>($count);
                 b.iter(|| bitmap.is_empty());
             },
         );
+    };
+}
+
+fn is_empty(c: &mut Criterion) {
+    let mut group = c.benchmark_group("IsEmpty");
+    for count in [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000].iter() {
+        bench_is_empty!(group, count, Roaring, u32);
+        bench_is_empty!(group, count, RoaringTwoLevels, u64);
+        bench_is_empty!(group, count, RoaringTreeMap, u64);
+        bench_is_empty!(group, count, RoaringLazy, u64);
     }
     group.finish();
 }
 
-fn remove(c: &mut Criterion) {
-    let mut group = c.benchmark_group("remove");
-    for count in [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000].iter() {
-        group.bench_with_input(
-            BenchmarkId::from_parameter(count),
-            count,
-            |b, &count| {
+macro_rules! bench_remove {
+    // Benchmark the removal of a single value.
+    ($group:ident, $count:ident, $roaring: ty, $int:ty) => {
+        $group.bench_with_input(
+            BenchmarkId::new(stringify!($roaring), $count),
+            $count,
+            |b, &$count| {
                 b.iter_batched(
-                    || random_bitmap(count),
+                    || random_bitmap::<$roaring, $int>($count),
                     |(mut bitmap, value)| bitmap.remove(black_box(value)),
                     BatchSize::SmallInput,
                 );
             },
         );
+    };
+}
+
+fn remove(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Remove");
+    for count in [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000].iter() {
+        bench_remove!(group, count, Roaring, u32);
+        bench_remove!(group, count, RoaringTwoLevels, u64);
+        bench_remove!(group, count, RoaringTreeMap, u64);
+        bench_remove!(group, count, RoaringLazy, u64);
     }
     group.finish();
 }
 
-fn get_input_values(count: i32, want_sorted: bool) -> Vec<u32> {
+/// Returns a list of random value, uniformly distributed and optionally sorted.
+fn get_input_values<I>(count: i32, want_sorted: bool) -> Vec<I>
+where
+    I: Ord,
+    Standard: Distribution<I>,
+{
     let mut prng = thread_rng();
-    let mut values = (0..count).map(|_| prng.gen::<u32>()).collect::<Vec<_>>();
+    let mut values = (0..count).map(|_| prng.gen::<I>()).collect::<Vec<_>>();
     if want_sorted {
         values.sort_unstable()
     }
     values
 }
 
-// Also returns a random value (can be used to test `contains`, `remove`, …)
-fn random_bitmap(cardinality: i32) -> (Roaring, u32) {
+/// Builds a bitmap randomly populated.
+///
+/// Also returns a random value (can be used to test `contains`, `remove`, …).
+fn random_bitmap<R, I>(cardinality: i32) -> (R, I)
+where
+    R: FromIterator<I>,
+    I: Ord + Copy,
+    Standard: rand::distributions::Distribution<I>,
+{
     let values = get_input_values(cardinality, true);
     let value = *values.choose(&mut thread_rng()).expect("non-empty values");
 
-    (values.into_iter().collect::<Roaring>(), value)
+    (values.into_iter().collect::<R>(), value)
 }
 
-criterion_group!(benches, insert, contains, cardinality, is_empty, remove);
+criterion_group!(
+    benches,
+    insert_loop,
+    insert_iter,
+    contains,
+    cardinality,
+    is_empty,
+    remove
+);
 
 criterion_main!(benches);
